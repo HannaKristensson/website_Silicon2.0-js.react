eb4c96df7ead62fbf3e0021bbda0479d9c20153d
Hanna Kristensson <hanna.kristensson@hotmail.com>
2024-10-25 10:00:15 +0200

    Copying over silicon-files to the react project

--- silicon_react/src/main.jsx
+++ silicon_react/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './assets/css/stylesheet'
+// import './assets/css/contact'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
