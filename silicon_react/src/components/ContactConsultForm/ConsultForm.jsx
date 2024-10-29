99de4e9576f861d693f74b17cbd0bf6de6190461
Hanna Kristensson <hanna.kristensson@hotmail.com>
2024-10-29 10:04:22 +0100

    different fixes

--- silicon_react/src/components/ContactConsultForm/ConsultForm.jsx
+++ silicon_react/src/components/ContactConsultForm/ConsultForm.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ConsultForm() {
   return (
-      <htmlForm action="" className="consult_htmlForm htmlForm_primary">
+      <form action="" className="consult_form form_primary">
         <label htmlFor="">Full name</label>
         <input type="text"/>
         <label htmlFor="">Email address</label>
@@ -13,7 +13,7 @@ function ConsultForm() {
           <option value="prog">Programmer</option>
         </select>
         <button className="btn_primary" id="consult_submit">Make an appointment</button>
-      </htmlForm>
+      </form>
   )
 }
 
--- silicon_react/src/components/HomeQuestions.jsx
+++ silicon_react/src/components/HomeQuestions.jsx
@@ -3,11 +3,11 @@ import text from '../assets/images/questions_text.svg'
 
 function Questions() {
   return (
-    <section class="page_question container padding-rl">
-        <div class="question_text">
-            <div class="question_title">
-                <h2 class="question_h2">Any questions?</h2>
-                <h2 class="question_h2">Check out the FAQs</h2>
+    <section className="page_question container padding-rl">
+        <div className="question_text">
+            <div className="question_title">
+                <h2 className="question_h2">Any questions?</h2>
+                <h2 className="question_h2">Check out the FAQs</h2>
             </div>
         <p>Still have unanswered questions and need to get in touch?</p>
         </div>
@@ -15,22 +15,22 @@ function Questions() {
         {/* accordion */}
 
         <a href="#">
-            <button class="btn_primary">Contact us now</button>
+            <button className="btn_primary">Contact us now</button>
         </a>
-        <div class="questions_contact">
-            <div class="questions_call-txt">
+        <div className="questions_contact">
+            <div className="questions_call-txt">
                 <img src={call} alt="Phone handset."/>
                 <p>Still have  questions?</p>
-                <a href="#" class="questions_nav" >Contact us
-                    <i class="fa-solid fa-arrow-right"></i>
+                <a href="#" className="questions_nav" >Contact us
+                    <i className="fa-solid fa-arrow-right"></i>
                 </a>
             </div>
-            <div class="questions_call-txt questions_txt" >
+            <div className="questions_call-txt questions_txt" >
                 <img src={text} alt="Chat bubble." />
                 <p>Don't like phone calls?</p>
-                <a href="#" class="questions_nav" >
+                <a href="#" className="questions_nav" >
                     Contact us
-                    <i class="fa-solid fa-arrow-right"></i>
+                    <i className="fa-solid fa-arrow-right"></i>
                 </a>
             </div>
         </div>
