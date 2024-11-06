d72e57819e48de6c4ad4ab6e94f866bc812bcb5f
Hanna Kristensson <hanna.kristensson@hotmail.com>
2024-11-06 12:01:04 +0100

    trubbleshot consultForm with validation, and made some css changes

--- silicon_react/src/components/ContactConsultForm/ConsultForm.jsx
+++ silicon_react/src/components/ContactConsultForm/ConsultForm.jsx
@@ -13,7 +13,6 @@ const ConsultForm = () => {
   const [formData, setFormData] = useState({ fullName: '', email: '', specialist: 'Options', });
   const [submissionStatus, setSubmissionStatus] = useState('');
   const [errors, setErrors] = useState({});
-  const [valids, setValids] = useState({});
 
 
   const validateField = (name, value) => {
@@ -31,21 +30,17 @@ const ConsultForm = () => {
 
   const validateForm = () => {
     const newErrors = {}
-    const newValids = {}
 
     if (!/^[A-Öa-ö\s\-]{2,}$/.test(formData.fullName)) {
       newErrors.fullName = 'Your name must be at least 2 characters long, no numbers.'
-    } else (!/^[A-Öa-ö\s\-]{2,}$/.test(formData.fullName)) {
-      newValids.fullName = 'Your name must be at least 2 characters long, no numbers.' 
-    }
+    } 
 
     if (!/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z0-9]{2,}$/.test(formData.email)) {
-      newErrors.email = 'Must be an vaild email-adress. (eg. aa@aa.aa)';
+      newErrors.email = 'Must be an vaild email-adress. (eg. aa@aa.aa)'
     }
 
-    setValids(newValids)
     setErrors(newErrors)
-    return Object.keys(newErrors).length === 0 && Object.keys(newValids).length === 0;
+    return Object.keys(newErrors).length === 0;
   }
 
 
@@ -63,35 +58,42 @@ const ConsultForm = () => {
     e.preventDefault();
 
     if (validateForm()) {
-      try {
-        const res = await fetch('https://win24-assignment.azurewebsites.net/api/forms/contact', {
-          method: 'POST',
-          headers: { 'content-Type': 'application/json'
-          },
-          body: JSON.stringify(formData)
-      });
+      const res = await fetch('https://win24-assignment.azurewebsites.net/api/forms/contact', 
+        {
+        method: 'POST',
+        headers: { 'content-Type': 'application/json'},
+        body: JSON.stringify(formData),
+        }
+      );
+    
 
       if (res.ok) {
         setSubmissionStatus('Thank you for your message!');
         setFormData({ fullName: '', email: '', specialist: 'Options' });
         console.log('Everything was successfull')
       } else {
-      setSubmissionStatus('Something went wrong, make sure to fill both textfields correctly.'
-      );
+      setSubmissionStatus('Something went wrong, make sure to fill both textfields correctly.');
       }
 
       console.log('status: ' + (res.status));
-
+      
+      
+      
       try {
         const data = await res.json();
         console.log(data);
+        return
       }
       catch {
         const data = await res.text();
         console.log(data);
       }
+    }
+      
+  }
+      
+
     
-  }}
 
   
   return (
@@ -99,7 +101,6 @@ const ConsultForm = () => {
         <label htmlFor="fullName">Full name</label>
         <input type="text" name="fullName" defaultValue={formData.fullName} onChange={handleInputChange} required />
         {errors.fullName && <span className='invalid_input'>{errors.fullName}</span>}
-        {valids.fullName && <span className='valid_input'>{valids.fullName}</span>}
 
         <label htmlFor="email">Email address</label>
         <input className="" type="email" name="email" defaultValue={formData.email} onChange={handleInputChange} required/>
@@ -109,10 +110,10 @@ const ConsultForm = () => {
         <select className="" id="specialistSelect" name="specialist" defaultValue={formData.specialist} onChange={handleInputChange} required>
           {options.map(option => ( <option key={option.id} value={option.id}>{option.text}</option>))}
         </select>
-        {submissionStatus && <p>{submissionStatus}</p>}
+        {submissionStatus && <p className='submission' >{submissionStatus}</p>}
         <button className="btn_primary" id="consult_submit">Make an appointment</button>
       </form>
   )
-  }}
+}
 
 export default ConsultForm
